d1e93b7ea6efb6a5e486f8e355dccfd220ce7f53
ardayz <72218302+ardayz@users.noreply.github.com>
2022-09-18 12:57:19 +0300

    Ui Connections & Cooldowns

--- Assets/Scripts/CemScripts/UIDynamicVariables.cs
+++ Assets/Scripts/CemScripts/UIDynamicVariables.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIDynamicVariables : MonoBehaviour
 {
@@ -28,6 +29,13 @@ public class UIDynamicVariables : MonoBehaviour
     public float playerAttackDashCooldown{get; private set;}
     public float playerSmashCooldown{get; private set;}
 
+    public Slider SevapOrb;
+    public Slider HealthOrb;
+    private bool isCoolDown1 = false;
+    private bool isCoolDown2 = false;
+    public Image CoolDown1;
+    public Image CoolDown2;
+    private float cooldowntime;
     void Start()
     {
         playerTransform = GameObject.Find("Player").transform;
@@ -35,6 +43,41 @@ public class UIDynamicVariables : MonoBehaviour
         smashSkill = playerTransform.GetComponent<SmashSkill>();
     }
 
+    void CoolDown()
+    {
+        if (playerDashCooldown >= 1 && isCoolDown1 == false )
+        {
+            cooldowntime = playerDashCooldown;
+            isCoolDown1 = true;
+            CoolDown1.fillAmount = 1; 
+        }
+
+        if (isCoolDown1)
+        {
+            CoolDown1.fillAmount -= 1 / cooldowntime * Time.deltaTime;
+            if (CoolDown1.fillAmount <= 0)
+            {
+                CoolDown1.fillAmount = 0;
+                isCoolDown1 = false;
+            }
+        }
+        if (playerSmashCooldown >= 2 && isCoolDown2 == false)
+        {
+            cooldowntime = playerSmashCooldown;
+            isCoolDown2 = true;
+            CoolDown2.fillAmount = 1;
+        }
+
+        if (isCoolDown2)
+        {
+            CoolDown2.fillAmount -= 1 / cooldowntime * Time.deltaTime;
+            if (CoolDown2.fillAmount <= 0)
+            {
+                CoolDown2.fillAmount = 0;
+                isCoolDown2 = false;
+            }
+        }
+    }
     void Update()
     {
         playerHP = PlayerHealthManager.Instance.getHealth();
@@ -46,6 +89,11 @@ public class UIDynamicVariables : MonoBehaviour
         playerAttackDashCooldown = dashSkill.getAttackCooldownCounter();
 
         playerSmashCooldown = smashSkill.getCooldownCounter();
+
+        CoolDown();
+
+        HealthOrb.value = playerHPSmooth;
+        SevapOrb.value = playerMana;
     }
 
 
